bc00cb1aad879a1c2cb0982f0568cd0cfe00b72e
Jay Kalinani <jaykalinani@gmail.com>
2023-11-29 15:37:42 -0500

    AsterX: Add a separate file for flux corrections code

---
+++ AsterX/src/flux_corrections.cxx
@@ -0,0 +1,93 @@
+#include <loop_device.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+
+#include "utils.hxx"
+
+namespace AsterX {
+using namespace std;
+using namespace Loop;
+using namespace Arith;
+
+
+template <int dir> void CalcFluxCorrections(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_AsterX_Flux_Corrections;
+  DECLARE_CCTK_PARAMETERS;
+  
+  /* grid functions for fluxes */
+  const vec<GF3D2<CCTK_REAL>, dim> fluxdenss{fxdens, fydens, fzdens};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomxs{fxmomx, fymomx, fzmomx};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomys{fxmomy, fymomy, fzmomy};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomzs{fxmomz, fymomz, fzmomz};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxtaus{fxtau, fytau, fztau};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxBxs{fxBx, fyBx, fzBx};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxBys{fxBy, fyBy, fzBy};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxBzs{fxBz, fyBz, fzBz};
+
+  const vec<GF3D2<CCTK_REAL>, dim> fluxdenss_HO{fxdens_HO, fydens_HO, fzdens_HO};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomxs_HO{fxmomx_HO, fymomx_HO, fzmomx_HO};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomys_HO{fxmomy_HO, fymomy_HO, fzmomy_HO};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxmomzs_HO{fxmomz_HO, fymomz_HO, fzmomz_HO};
+  const vec<GF3D2<CCTK_REAL>, dim> fluxtaus_HO{fxtau_HO, fytau_HO, fztau_HO};
+ 
+  // Face-centred grid functions (in direction `dir`)
+  constexpr array<int, dim> face_centred = {!(dir == 0), !(dir == 1),
+                                            !(dir == 2)};
+
+  grid.loop_int_device<
+      face_centred[0], face_centred[1],
+      face_centred
+          [2]>(grid.nghostzones, [=] CCTK_DEVICE(
+                                     const PointDesc
+                                         &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+  // Update correction grid functions:
+
+    if ( (correction_order != 2) && (correction_order != 4) && (correction_order != 6))
+    {
+       printf("Incorrect correction order for the fluxes! \n");
+       assert(0);
+    }
+
+    fluxdenss_HO(dir)(p.I) = higher_order_correction(fluxdenss(dir), p, dir, correction_order);
+    fluxmomxs_HO(dir)(p.I) = higher_order_correction(fluxmomxs(dir), p, dir, correction_order);
+    fluxmomys_HO(dir)(p.I) = higher_order_correction(fluxmomys(dir), p, dir, correction_order);
+    fluxmomzs_HO(dir)(p.I) = higher_order_correction(fluxmomzs(dir), p, dir, correction_order);
+    fluxtaus_HO(dir)(p.I) = higher_order_correction(fluxtaus(dir), p, dir, correction_order);
+
+  });
+
+  // Update flux grid functions with computed corrections
+  grid.loop_int_device<
+      face_centred[0], face_centred[1],
+      face_centred
+          [2]>(grid.nghostzones, [=] CCTK_DEVICE(
+                                     const PointDesc
+                                         &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+
+    fluxdenss(dir)(p.I) += fluxdenss_HO(dir)(p.I);
+    fluxmomxs(dir)(p.I) += fluxmomxs_HO(dir)(p.I);
+    fluxmomys(dir)(p.I) += fluxmomys_HO(dir)(p.I);
+    fluxmomzs(dir)(p.I) += fluxmomzs_HO(dir)(p.I);
+    fluxtaus(dir)(p.I) += fluxtaus_HO(dir)(p.I);
+
+  });
+
+}
+
+extern "C" void AsterX_Flux_Corrections(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_AsterX_Flux_Corrections;
+  DECLARE_CCTK_PARAMETERS;
+
+  CalcFluxCorrections<0>(cctkGH);
+  CalcFluxCorrections<1>(cctkGH);
+  CalcFluxCorrections<2>(cctkGH);
+}
+
+} // namespace AsterX
