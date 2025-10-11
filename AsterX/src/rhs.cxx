8bcc91dab34b8be711e02165fabcd87c49aef831
Allen Wen <acw6923@g.rit.edu>
2025-10-11 00:35:33 -0400

    ReconX: add WENOZ+ (with FIL mod) as option

--- AsterX/src/rhs.cxx
+++ AsterX/src/rhs.cxx
@@ -32,6 +32,8 @@ extern "C" void AsterX_RHS(CCTK_ARGUMENTS) {
     reconstruction = reconstruction_t::eppm;
   else if (CCTK_EQUALS(reconstruction_method, "wenoz"))
     reconstruction = reconstruction_t::wenoz;
+  else if (CCTK_EQUALS(reconstruction_method, "wenozp"))
+    reconstruction = reconstruction_t::wenozp;
   else if (CCTK_EQUALS(reconstruction_method, "mp5"))
     reconstruction = reconstruction_t::mp5;
   else
--- ReconX/src/reconstruct.hxx
+++ ReconX/src/reconstruct.hxx
@@ -12,6 +12,7 @@
 #include "ppm.hxx"
 #include "eppm.hxx"
 #include "wenoz.hxx"
+#include "wenozp.hxx"
 #include "mp5.hxx"
 
 #include <type_traits>
@@ -31,6 +32,7 @@ enum class reconstruction_t {
   ppm,
   eppm,
   wenoz,
+  wenozp,
   mp5
 };
 
@@ -84,6 +86,15 @@ reconstruct(const GF3D2<const CCTK_REAL> &gf_var, const PointDesc &p,
   case reconstruction_t::wenoz: {
     tmp = wenoz_reconstruct(gf_var(Immm), gf_var(Imm), gf_var(Im), gf_var(Ip),
                              gf_var(Ipp), gf_var(Ippp),
+                             reconstruct_params.weno_eps);
+    break;
+  }
+
+  case reconstruction_t::wenozp: {
+		// const CCTK_REAL dx = p.DX[dir];
+		const CCTK_REAL dx = 0.0;
+    tmp = wenozp_reconstruct(gf_var(Immm), gf_var(Imm), gf_var(Im), gf_var(Ip),
+                             gf_var(Ipp), gf_var(Ippp), dx,
                              reconstruct_params.weno_eps);
     break;
   }
