f3078888ec3235dc764ba193c1255c93fdaeba50
Allen Wen <acw6923@g.rit.edu>
2026-09-01 14:11:59 -0500

    AsterX: revert full sync of dBstag and dB

--- AsterX/src/sync.cxx
+++ AsterX/src/sync.cxx
@@ -97,25 +97,17 @@ extern "C" void AsterX_ProlongatedBstag(CCTK_ARGUMENTS) {
 }
 
 extern "C" void AsterX_CommdBstag(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_PARAMETERS;
   static const std::vector<int> groups = {CCTK_GroupIndex("AsterX::dBx_stag"),
                                           CCTK_GroupIndex("AsterX::dBy_stag"),
                                           CCTK_GroupIndex("AsterX::dBz_stag")};
 
-  if (use_subcycling)
-    SyncGroupsByDirISubcycling(cctkGH, groups.size(), groups.data(), nullptr);
-  else
-    SyncGroupsByDirI(cctkGH, groups.size(), groups.data(), nullptr);
+  SyncGroupsByDirIGhostOnly(cctkGH, groups.size(), groups.data(), nullptr);
 }
 
 extern "C" void AsterX_CommdB(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_PARAMETERS;
   static const std::vector<int> groups = {CCTK_GroupIndex("AsterX::dB")};
 
-  if (use_subcycling)
-    SyncGroupsByDirISubcycling(cctkGH, groups.size(), groups.data(), nullptr);
-  else
-    SyncGroupsByDirI(cctkGH, groups.size(), groups.data(), nullptr);
+  SyncGroupsByDirIGhostOnly(cctkGH, groups.size(), groups.data(), nullptr);
 }
 
 } // namespace AsterX
