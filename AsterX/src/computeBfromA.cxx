b491cef682590b9e0b0428c2b995f82c355d920b
Allen Wen <acw6923@g.rit.edu>
2026-09-16 17:05:33 -0500

    AsterX: clean up comments in computeBfromA

--- AsterX/src/computeBfromA.cxx
+++ AsterX/src/computeBfromA.cxx
@@ -159,40 +159,6 @@ template <int dir> void ComputeStaggeredPointValB(CCTK_ARGUMENTS) {
   grid.loop_int_device<face_centred[0], face_centred[1], face_centred[2]>(
      grid.nghostzones,
       [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        // const auto pint =  
-        //     vect<int,dim>((p.I < p.loop_max - 1) && (p.I > p.loop_min));
-        // Temporarily use flux GF as helper
-        if (dir == 0) {
-          dBx_stag(p.I) = dBx_stag_fa(p.I) - one_over_24 * laplace_perp<0>(fxdens, p);
-          //dBx_stag(p.I) = dBx_stag_fa(p.I) - one_over_24 * (pint[1]*laplace_1d<1>(fxdens, p) + pint[2]*laplace_1d<2>(fxdens, p));
-        } else if (dir == 1) {
-          dBy_stag(p.I) = dBy_stag_fa(p.I) - one_over_24 * laplace_perp<1>(fydens, p);
-          //dBy_stag(p.I) = dBy_stag_fa(p.I) - one_over_24 * (pint[0]*laplace_1d<0>(fydens, p) + pint[2]*laplace_1d<2>(fydens, p));
-        } else if (dir == 2) {
-          dBz_stag(p.I) = dBz_stag_fa(p.I) - one_over_24 * laplace_perp<2>(fzdens, p);
-          //dBz_stag(p.I) = dBz_stag_fa(p.I) - one_over_24 * (pint[0]*laplace_1d<0>(fzdens, p) + pint[1]*laplace_1d<1>(fzdens, p));
-        }
-      });
-}
-
-/*
-template <int dir> void ComputeStaggeredPointValB_Int(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_ARGUMENTSX_AsterX_ComputedBstagIter;
-  DECLARE_CCTK_PARAMETERS;
-
-  static_assert(dir >= 0 && dir < 3, "");
-
-  constexpr array<int, dim> face_centred = {!(dir == 0), !(dir == 1),
-                                            !(dir == 2)};
-
-  // Here, we have already computed the face averaged <dBi_stag> from <Avec>.
-  // Now, we use Equation 21 of https://arxiv.org/pdf/2310.11831 to compute the point value
-  // dBi_stag.
-  constexpr CCTK_REAL one_over_24 = CCTK_REAL(1)/CCTK_REAL(24);
-  grid.loop_int_device<face_centred[0], face_centred[1], face_centred[2]>(
-     grid.nghostzones,
-      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        // Temporarily use flux GF as helper
         if (dir == 0) {
           dBx_stag(p.I) = dBx_stag_fa(p.I) - one_over_24 * laplace_perp<0>(fxdens, p);
         } else if (dir == 1) {
@@ -202,7 +168,6 @@ template <int dir> void ComputeStaggeredPointValB_Int(CCTK_ARGUMENTS) {
         }
       });
 }
-*/
 
 extern "C" void AsterX_ComputedBstagFromA(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_AsterX_ComputedBstagFromA;
@@ -259,10 +224,6 @@ extern "C" void AsterX_DecdBstagIter(CCTK_ARGUMENTS) {
                                       CCTK_GroupIndex("AsterX::dBz_stag")};
 
   SyncGroupsByDirIGhostOnly(cctkGH, groups.size(), groups.data(), nullptr);
-  // int minghosts = min(cctk_nghostzones[0], min(cctk_nghostzones[1], cctk_nghostzones[2]));
-  // if (*dBstag_pv_iter==0 || ((n_dBstagpv_iters - *dBstag_pv_iter) % minghosts == 0))  {
-  //   CCTK_VINFO("Communicating dBstag at solve iter %d", *dBstag_pv_iter);
-  // }
 }
 /* End Point Value dBstag Calculation */
 
@@ -282,19 +243,6 @@ extern "C" void AsterX_ComputedBFromdBstag(CCTK_ARGUMENTS) {
         dBy(p.I) = calc_avg_f2c(dBy_stag, p, 1, ordL);
         dBz(p.I) = calc_avg_f2c(dBz_stag, p, 2, ordL);
       });
-
-  // Interpolate dB in boundaries/ghosts at lower order
-  // if (nloop != 0) {
-  //   grid.loop_outer_n_device<1, 1, 1>(
-  //       grid.nghostzones, nloop,
-  //       [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-  //         /* Interpolation of staggered B components to cell center
-  //          */
-  //         dBx(p.I) = calc_avg_f2c(dBx_stag, p, 0, 2);
-  //         dBy(p.I) = calc_avg_f2c(dBy_stag, p, 1, 2);
-  //         dBz(p.I) = calc_avg_f2c(dBz_stag, p, 2, 2);
-  //       });
-  // }
 }
 
 extern "C" void AsterX_ComputeBFromdB(CCTK_ARGUMENTS) {
